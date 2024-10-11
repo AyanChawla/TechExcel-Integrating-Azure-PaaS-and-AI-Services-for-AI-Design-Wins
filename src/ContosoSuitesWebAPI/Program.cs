8460c0587f6252773e383918dd4819f2bf660ce9
Ayan Chawla <ayanchawla@microsoft.com>
2024-10-11 17:51:24 +0530

    Final Code

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -1,4 +1,8 @@
-using Azure.Identity;
+/// <summary>
+/// Configures and runs the Contoso Suites Web API application.
+/// </summary>
+/// <param name="args">The command-line arguments.</param>
+/// /// using Azure.Identity;
 using Microsoft.Azure.Cosmos;
 using ContosoSuitesWebAPI.Agents;
 using ContosoSuitesWebAPI.Entities;
@@ -42,14 +46,19 @@ builder.Services.AddSingleton<Kernel>((_) =>
         IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
         kernelBuilder.AddAzureOpenAIChatCompletion(
             deploymentName: builder.Configuration["AzureOpenAI:DeploymentName"]!,
-            endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
-            apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
+            endpoint: builder.Configuration["ApiManagement:Endpoint"]!,
+            apiKey: builder.Configuration["ApiManagement:ApiKey"]!/*,
+            modelId: "2024-06-01"*/
+            /*endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
+            apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!*/
         );
         #pragma warning disable SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
         kernelBuilder.AddAzureOpenAITextEmbeddingGeneration(
         deploymentName: builder.Configuration["AzureOpenAI:EmbeddingDeploymentName"]!,
-        endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
-        apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
+        endpoint: builder.Configuration["ApiManagement:Endpoint"]!,
+        apiKey: builder.Configuration["ApiManagement:ApiKey"]!
+        /*endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
+        apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!*/
         );
         #pragma warning restore SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
