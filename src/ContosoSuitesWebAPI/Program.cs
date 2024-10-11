78b07b46d20a700ffd825ba6ae6e51648fcbf9e0
Ayan Chawla <ayanchawla@microsoft.com>
2024-10-11 15:48:01 +0530

    Ex 5

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -45,10 +45,29 @@ builder.Services.AddSingleton<Kernel>((_) =>
             endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
             apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
         );
+        #pragma warning disable SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        kernelBuilder.AddAzureOpenAITextEmbeddingGeneration(
+        deploymentName: builder.Configuration["AzureOpenAI:EmbeddingDeploymentName"]!,
+        endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
+        apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
+        );
+        #pragma warning restore SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
         kernelBuilder.Plugins.AddFromType<DatabaseService>();
+        kernelBuilder.Plugins.AddFromType<MaintenanceRequestPlugin>("MaintenanceCopilot");
+
+        kernelBuilder.Services.AddSingleton<CosmosClient>((_) =>
+        {
+            CosmosClient client = new(
+                connectionString: builder.Configuration["CosmosDB:ConnectionString"]!
+            );
+            return client;
+        });
+
         return kernelBuilder.Build();
     });
 
+/*
 builder.Services.AddSingleton<AzureOpenAIClient>((_) =>
 {
     var endpoint = new Uri(builder.Configuration["AzureOpenAI:Endpoint"]!);
@@ -57,7 +76,7 @@ builder.Services.AddSingleton<AzureOpenAIClient>((_) =>
     var client = new AzureOpenAIClient(endpoint, credentials);
     return client;
 });
-
+*/
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -136,7 +155,9 @@ app.MapPost("/VectorSearch", async ([FromBody] float[] queryVector, [FromService
 app.MapPost("/MaintenanceCopilotChat", async ([FromBody]string message, [FromServices] MaintenanceCopilot copilot) =>
 {
     // Exercise 5 Task 2 TODO #10: Insert code to call the Chat function on the MaintenanceCopilot. Don't forget to remove the NotImplementedException.
-    throw new NotImplementedException();
+    //throw new NotImplementedException();
+    var response = await copilot.Chat(message);
+    return response;
 })
     .WithName("Copilot")
     .WithOpenApi();
